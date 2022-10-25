955de78d18769a0f80baaae425ecafa8c2179464
Gennady <pospelov@Gennadys-MBP.lan>
2022-10-25 12:25:10 +0300

    rewriting

--- web/static/js/js_main.js
+++ web/static/js/js_main.js
@@ -2,7 +2,9 @@
   ("use strict");
 
   let ajaxResult = "";
-  let inputHandlerResult = "";
+  let inputHandlerResult = [];
+  let currentList = [];
+  let currentTags = [];
 
   const ajaxRequest = (url, method, data) => {
     return new Promise((resolve, reject) => {
@@ -25,134 +27,158 @@
     });
   };
 
-  const inputHandler = () => {
+  const inputHandler = (inputHandlerResult) => {
+    let focus = true;
+    let currentValue = "";
     const requestInput = document.getElementById("id_location01");
+
     //find delimeter and clear input
     requestInput.addEventListener("input", (event) => {
-      let value = event.target.value;
-      let lastChar = value.slice(-1);
+      currentValue = requestInput.value;
+      console.log(currentValue);
+      let lastChar = currentValue.slice(-1);
       let delimeterFound =
         lastChar === " " || lastChar === "," || lastChar === "\n";
 
       if (delimeterFound) {
+        console.log("delimeter found");
+        inputHandlerResult.push(currentValue.slice(0, -1));
+        renderDropdown(inputHandlerResult, "pre");
+        //clear input
         requestInput.value = "";
-        inputHandlerResult = value.slice(0, -1);
-        buildAndRenderDropDown(ajaxResult, inputHandlerResult);
+        currentValue = "";
       }
+      dropdownVisibility(currentValue, focus);
     });
+
+    //if focused paint the box borders, repaint back on focusout
+
+    const border = document.querySelector(
+      "#request_container span .border_box"
+    );
+
+    requestInput.addEventListener("focus", (event) => {
+      border.style.border = "1px solid var(--blue)";
+      focus = true;
+      dropdownVisibility(currentValue, focus);
+    });
+    /* requestInput.addEventListener("focusout", (event) => {
+      border.classList.remove("focused");
+      border.style.border = "";
+      focus = false;
+      dropdownVisibility(currentValue, focus);
+    }); */
   };
 
-  const renderDropDown = () => {
-    const requestInput = document.getElementById("id_location01");
+  //show/hide dropdown list
+  const dropdownVisibility = (focus) => {
+    const dropdown = document.querySelector("#dropdown .list");
+    //check if dropdown is currently active
+    //console.log("currentInput:", currentInput.length);
+    //console.log(focus);
 
-    //show/hide dropdown list
-    function showDropdown() {
-      let dropdown = document.querySelector("#dropdown");
-      let isActive = dropdown.classList === "active";
-
-      dropdown.classList = isActive ? "hidden" : "active";
-
-      /*
-            setting textarea height if dropdown is active
-            to prevent dropdown closing while clicking on tags from list
-            */
-
-      //calculate list height
-      let cssObj = window.getComputedStyle(dropdown);
-      let dropdownHeight = cssObj.getPropertyValue("height");
-
-      requestInput.style.height = isActive ? "" : dropdownHeight;
+    if (focus) {
+      dropdown.classList.remove("hidden");
+    } else {
+      dropdown.classList.add("hidden");
     }
-
-    //event listeners
-    requestInput.addEventListener("click", showDropdown);
-    requestInput.addEventListener("input", showDropdown);
-    requestInput.addEventListener("blur", showDropdown);
   };
 
-  const buildAndRenderDropDown = (inputData, inputHandlerResult) => {
-    const selectDropdown = document.querySelector("#dropdown");
-    selectDropdown.innerHTML = "";
+  const renderDropdown = (data, param) => {
+    const selectList = document.querySelector("#dropdown .list");
+    //selectList.innerHTML = "";
 
-    const isInitialData = inputData !== undefined && inputData !== null;
-    const isHandlerResult =
-      inputHandlerResult !== undefined && inputHandlerResult !== null;
+    console.log(data);
 
-    const createItem = (index) => {
-      let newLoopItem = document.createElement("span");
-      let newListItem = document.createElement("span");
-      let loopItemText = document.createElement("span");
-      let listItemText = document.createElement("span");
-      loopItemText.classList.add("list_text");
-      listItemText.classList.add("list_text");
-      let checkmark = document.createElement("span");
-      checkmark.classList.add("checkmark");
+    const createItem = (index, element, input) => {
+      let isInList = currentList.includes(input[index]);
 
-      //handle clicks on items
-      const itemsClickHandler = (item) => {
-        item.addEventListener("click", function (event) {
-          let noCheckmark = event.target.children.length === 0;
-          noCheckmark ? item.append(checkmark) : "";
-          event.currentTarget.classList.toggle("selected");
-        });
-      };
-
-      /* if there is a loop for items create new item for each one, 
-      if single - put a result from inputHandler()*/
-      if (index !== null) {
-        loopItemText.innerText = inputData[index];
-        newLoopItem.append(loopItemText);
-        selectDropdown.append(newLoopItem);
-        itemsClickHandler(newLoopItem);
+      if (isInList) {
+        return;
       } else {
-        listItemText.innerText = inputHandlerResult;
-        newListItem.append(listItemText);
-        selectDropdown.prepend(newListItem);
-        itemsClickHandler(newListItem);
+        let newItem = document.createElement("span");
+        let itemText = document.createElement("span");
+        itemText.classList.add("list_text");
+        let checkmark = document.createElement("span");
+        checkmark.classList.add("checkmark");
+
+        //handle clicks on items
+        const itemsClickHandler = (item) => {
+          item.addEventListener("click", function (event) {
+            let noCheckmark = event.target.children.length === 0;
+            noCheckmark ? item.append(checkmark) : "";
+            event.currentTarget.classList.toggle("selected");
+            //focus on input when item clicked
+            const requestInput = document.getElementById("id_location01");
+            requestInput.focus();
+            console.log(currentTags.includes(item.innerText));
+          });
+        };
+
+        itemText.innerText = input[index];
+        newItem.append(itemText);
+        selectList.append(newItem);
+        if (param === "pre") selectList.prepend(newItem);
+        itemsClickHandler(newItem);
+        currentList.push(input[index]);
       }
     };
 
-    //generate items for dropdown from inital data
-    if (isInitialData) {
-      for (let i = 0; i < inputData.length; i++) {
-        createItem(i);
-      }
-    }
-    //if there is new input tag
+    //generate items for dropdown
 
-    if (isHandlerResult) {
-      createItem(null);
-    }
+    data.forEach((element, index) => {
+      console.log(index);
+      createItem(index, element, data);
+    });
+  };
 
-    //handle dropdown visibility behavior
-    tagHandler();
-    renderDropDown();
+  const checkTagsWidth = (exceeded) => {
+    let box = document.querySelector("#request_container .tag_box");
+    let tagsSpan = document.querySelector("#request_container .tag_items");
+    let tagItems = [...tagsSpan.children];
+    let boxWidth = parseInt(getComputedStyle(box).width);
+    let currentWidth = 0;
+
+    tagItems.forEach((tag) => {
+      console.log(tag, typeof tag);
+      let tagWidth = getComputedStyle(tag).width;
+      currentWidth += parseInt(tagWidth.slice(0, -2));
+    });
+
+    exceeded = currentWidth >= boxWidth * 0.8;
+    /*  console.log(currentWidth, typeof currentWidth);
+    console.log("available width", boxWidth * 0.8);
+    console.log(tagItems);
+    console.log(exceeded); */
   };
 
   const tagHandler = () => {
     //new tag in input box from clicking on list item
-    const selectListItems = document.querySelectorAll(
-      "#dropdown span.list_text"
-    );
+
+    const selectListItems = document.querySelectorAll("#dropdown .list_text");
     const itemsArray = [...selectListItems];
+    console.log(itemsArray);
 
     //handle clicks on list item to create a tag in a box
     itemsArray.forEach((item) => {
       item.parentElement.addEventListener("click", function (event) {
-        let initial = document.querySelectorAll(".tag_text");
-        let current = [...initial];
-        let exists = false;
+        let notExists = true;
 
-        if (current.length > 0) {
-          current.forEach((tag) => {
-            if (tag.innerHTML === item.innerHTML) {
-              tag.parentElement.remove();
-              exists = true;
+        if (currentTags.length > 0) {
+          currentTags.forEach((tag) => {
+            if (currentTags.includes(tag)) {
+              item.parentElement.remove();
+              notExists = false;
             }
           });
         }
 
-        if (!exists) {
+        if (notExists) {
+          const selectTagSpan = document.querySelector(
+            "#request_container .tag_items"
+          );
+          const tagsSpanArray = [...selectTagSpan];
+
           let newTag = document.createElement("span");
           newTag.classList.add("tag");
 
@@ -166,10 +192,12 @@
           cross.innerText = "✕";
           newTag.append(cross);
 
-          let selectTagSpan = document.querySelector(
-            "#request_container .tag_items"
-          );
           selectTagSpan.append(newTag);
+          currentTags.push(newTag);
+
+          //check if width is exceeded
+          let exceeded = false; //by default
+          checkTagsWidth(exceeded);
 
           //remove tag from box on click at X sign
 
@@ -177,7 +205,7 @@
             event.currentTarget.parentElement.remove();
             //find selected row in list and remove 'selected' class
             const selectListItems = document.querySelectorAll(
-              "#dropdown span.list_text"
+              "#dropdown .list_text"
             );
             const itemsArray = [...selectListItems];
             itemsArray.forEach((item) => {
@@ -194,8 +222,8 @@
     ajaxRequest("http://127.0.0.1:8000/api/categories_json/", "GET").then(
       (result) => {
         ajaxResult = result["data"];
-        buildAndRenderDropDown(ajaxResult, null);
-        inputHandler();
+        renderDropdown(ajaxResult, currentList);
+        inputHandler(currentList, inputHandlerResult);
       }
     );
   });
